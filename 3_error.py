289af43f7cc396b93a92b1d6c8f4506f82fb611f
Kelvin-Data <bhlohass@gmail.com>
2022-04-02 06:36:59 +0800

    initial commit

---
+++ 3_error.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__) # help flask find the directory & file
+
+# Create a route decorator
+@app.route('/') # url www.about.html
+
+# def index():
+#    return "<h1>Hello World!</h1>"
+
+def index():
+    first_name = 'John'
+     # stuff = 'This is <strong>Bold</strong> Text!' # for the striptags
+    stuff = 'This is bold Text!'  # for the title
+
+    favourite_pizza = ['Pepperoni', 'Cheese', 'Mushroom', 41]
+    return render_template('index.html', 
+                first_name=first_name,
+                stuff=stuff,
+                favourite_pizza=favourite_pizza)
+    
+# localhost:5000/user/John or http://127.0.0.1:5000  # the dyanmic name
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template('user.html', user_name=name) # first name from user.html 
+                                                        # second name from definition
+
+
+# Create a custom error pages
+# Invalid URL pages
+@app.errorhandler(404)
+
+def page_not_found(e):
+    return render_template('404.html'),404
+
+# Internal server error
+@app.errorhandler(500)
+
+def page_not_found(e):
+    return render_template('505.html'),500
+
+# Powershell
+# $env:FLASK_APP = "3_error.py" 
+# $env:FLASK_ENV = "development"
+# flask run
+# venv_flask\Scripts\activartevenv\Scripts\activate
+
+# deactivate
+# cd ~/
+# pwd
+# mkdir .ssh
+# cd .ssh
+# pwd
+# ssh-keygen.exe
+# ls
+# cat id_rsa.pub
+
+# INITIALISE THE GITHUB
+# git config --global user.name "Kelvin-Data"
+# git config --global user.email "bhlohass@gmail.com"
+# git config --global push.default matching
+# git config --global alias.co checkout
+# git init
